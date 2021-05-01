a42f34e47d5b810de6e0c05e813abf9b3769738b
beautiful-inheritance-1081 <sduvly@gmail.com>
2021-05-01 18:11:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string.toLowerCase()){
     return "I can\'t hear you!"
-  } else if (string.toUpperCase()){
-    return "YES INDEED!"
-  }else{
+  } else if (string.toLowerCase() === "I love you, Grandma."{
     return "I love you, too."
+  }else{
+    return "YES INDEED!"
   }
   }
